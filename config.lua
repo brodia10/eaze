ec2a2d400ddf049439e10530108b40cee72564a3
Brook Rodia <brookrodia@gmail.com>
2022-06-18 18:01:58 -0400

    Remove duplicate toggleterm setup

--- config.lua
+++ config.lua
@@ -210,10 +210,6 @@ vim.cmd([[
 
 ]])
 
-require("toggleterm").setup {
-  size = 20,
-  open_mapping = [[<c-\>]],
-}
 vim.opt.termguicolors = true
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
